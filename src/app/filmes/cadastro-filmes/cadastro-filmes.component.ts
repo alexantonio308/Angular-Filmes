c439dc179abe7cbe996854b6201e8e470add08d6
AlexAntonio <alexantonio308@gmail.com>
2020-12-30 15:33:13 -0300

    Retorno do modal

--- src/app/filmes/cadastro-filmes/cadastro-filmes.component.ts
+++ src/app/filmes/cadastro-filmes/cadastro-filmes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import { MatDialog } from '@angular/material';
+import { Router } from '@angular/router';
 import { FilmesService } from 'src/app/core/filmes.service';
 import { AlertaComponent } from 'src/app/shared/components/alerta/alerta.component';
 import { ValidarCamposService } from 'src/app/shared/components/campos/validar-campos.service';
@@ -20,6 +21,7 @@ export class CadastroFilmesComponent implements OnInit {
               public dialog: MatDialog,
               private fb: FormBuilder,
               private filmeService: FilmesService,
+              private router: Router,
      ) { }
 
     get f() {
@@ -60,11 +62,25 @@ export class CadastroFilmesComponent implements OnInit {
           possuirBtnFechar: true
          } as Alerta
       };
-      const dialogRef= this.dialog.open(AlertaComponent, config);
+      const dialogRef = this.dialog.open(AlertaComponent, config);
+      dialogRef.afterClosed().subscribe((opcao: boolean) => {
+        if (opcao) {
+            this.router.navigateByUrl('filmes');
+          } else {
+            this.reiniciarForm();
+          }
+        });
     },
     () => {
-      alert('ERRO AO SALVAR');
-    });
+      const config = {
+        data: {
+          titulo: 'Erro ao salvar o registro!',
+          descricao: 'Não conseguimos salvar seu registro, favor tentar novamente mais tarde',
+          corBtnSucesso: 'warn',
+          btnSucesso: 'Fechar',
+         } as Alerta
+    };
+    this.dialog.open(AlertaComponent, config);
+  });
   }
 }
-
